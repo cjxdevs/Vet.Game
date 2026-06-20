e132d1b409497312f82bef197adda1ed8991f065
cjxdevs <cjxdevs@gmail.com>
2026-06-20 16:10:52 +0530

    Add files via upload

---
+++ 20 june 2026.py
@@ -0,0 +1,9 @@
+import random
+animal == random.randint(1, 100)
+if:
+    animal == 1 
+    print("the animal you have to treat is a horse")
+    animal == 2 
+    print("the animal you have to treat is a dog")
+
+
